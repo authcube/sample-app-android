cf150fc792db8606f9e6c1e7a412c6b4fec3485c
Vitor Cardoso <vitor.cardoso@sec4you.com.br>
2025-01-27 16:10:26 -0300

    add Strong/TOTP screens

---
+++ app/src/main/java/br/com/sec4you/authfy/app/StrongScreen.kt
@@ -0,0 +1,125 @@
+package br.com.sec4you.authfy.app
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import br.com.sec4you.authfy.app.ui.screens.home.HomeFooter
+import br.com.sec4you.authfy.app.ui.theme.AuthfySampleTheme
+import br.com.sec4you.authfy.app.ui.theme.BtnBg
+import br.com.sec4you.authfy.app.ui.theme.BtnTxt
+
+
+@Composable
+fun StrongScreen(navController: NavController) {
+  val TAG = "AUTHCUBE:STRONG_SCREEN"
+  val currentRoute by remember { mutableStateOf("strong") }
+
+  Scaffold(
+    topBar = { TopHomeBar() },
+    bottomBar = { HomeFooter(navController = navController, currentRoute = currentRoute) }
+  ) { innerPadding ->
+    Box(
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(innerPadding)
+    ) {
+      Column(
+        modifier = Modifier
+          .fillMaxSize()
+          .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+
+        Button(
+          onClick = { navController.navigate(Screen.TOTPScreen.route) },
+          shape = RoundedCornerShape(12.dp),
+          colors = ButtonDefaults.buttonColors(
+            containerColor =BtnBg,
+            contentColor =BtnTxt,
+          ),
+          contentPadding = PaddingValues(0.dp),
+          modifier = Modifier
+            .fillMaxWidth()
+            .height(160.dp)
+        ) {
+          Row(
+            modifier = Modifier
+              .fillMaxWidth()
+              .padding(start = 20.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+          ) {
+            Image(
+              painter = painterResource(R.drawable.timer_icon),
+              contentDescription = "Tokens",
+              modifier = Modifier.size(28.dp)
+            )
+            Spacer(
+              modifier = Modifier.width(
+                8.dp
+              )
+            )
+
+            Column {
+              Text(
+                text = "TOTP",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+              )
+              Text(
+                text = "Time-based One-Time Password",
+                fontSize = 14.sp,
+                color = Color.Gray
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun StrongScreenPreview() {
+  AuthfySampleTheme {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+      StrongScreen(navController = rememberNavController())
+    }
+  }
+}
